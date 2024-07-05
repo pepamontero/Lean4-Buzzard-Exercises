3a26a3d822d506178adaef70b048c61af2a473df
Pepa Montero Jimena <pepamonterojimena@gmail.com>
2024-07-05 13:34:29 +0200

    finished sets!

--- Lean4BuzzardExercises/Sets/sets6.lean
+++ Lean4BuzzardExercises/Sets/sets6.lean
@@ -75,11 +75,67 @@ example : id '' S = S := by
   rfl
 
 
--- Now let's try composition.
 variable (Z : Type) (g : Y → Z) (U : Set Z)
 
--- preimage of preimage is preimage of comp
-example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by sorry
+example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by
+  ext x
+  constructor <;> intro h
 
--- preimage of preimage is preimage of comp
-example : g ∘ f '' S = g '' (f '' S) := by sorry
+  /-    =>
+  We know that
+  `x ∈ (g ∘ f) ⁻¹ (U) = {x : X | (g ∘ f) (x) ∈ U}`
+  and we want to see that
+  `x ∈ f⁻¹(g⁻¹(U)) = {x : X | f x ∈ g⁻¹(U)} = {x : X | f x ∈ {y : Y | g y ∈ U}}`
+  Thus, what we really need is `f x ∈ g⁻¹(U)`, or `(g ∘ f) x ∈ U`
+  But this follows from the fact that `x ∈ (g ∘ f)⁻¹ (U)`
+  -/
+  apply h
+
+  /-    <=
+  Similar -/
+  apply h
+
+-- NOTAS
+/-
+  Sabemos que:
+  f : X → Y
+  g : Y → Z
+  g ∘ f : X → Z
+
+  U ⊆ Z-/
+
+  /-
+  f(S) = {y : Y | ∃ x : X, x ∈ S ∧ f x = y}
+  f⁻¹(T) = {x : X | f x ∈ T}
+  -/
+
+
+example : g ∘ f '' S = g '' (f '' S) := by
+  ext z
+  constructor <;> intro h
+
+  /- =>
+  We know `x ∈ (g ∘ f) (S)`
+  We want to know if `x ∈ g(f(S))`, with
+  g(f(S)) = {z : Z | ∃ y : Y, y ∈ f(S) ∧ g y = z} =
+    {z : Z | ∃ y : Y, y ∈ {y : Y | ∃ x : X, x ∈ S ∧ f x = y} ∧ g y = z} =
+    {z : Z | ∃ x : X, x ∈ S ∧ g (f x) = z}
+  -/
+  cases' h with x hx
+  use f x
+  constructor
+  use x
+  constructor
+  exact hx.left
+  rfl
+  exact hx.right
+
+  -- <= (Similar)
+  cases' h with y hy
+  cases' hy with hy1 hy2
+  cases' hy1 with x hx
+  use x
+  constructor
+  exact hx.left
+  rw [← hx.right] at hy2
+  exact hy2
