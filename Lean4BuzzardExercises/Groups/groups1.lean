5aabb75fb64383b47239be4eaa0499912bb9b4d4
Pepa Montero Jimena <pepamonterojimena@gmail.com>
2024-07-06 16:51:50 +0200

    add notes

--- Lean4BuzzardExercises/Groups/groups1.lean
+++ Lean4BuzzardExercises/Groups/groups1.lean
@@ -11,31 +11,38 @@ example (g : G) : g⁻¹ * g = 1 :=
   inv_mul_self g
 
 example (a b c : G) : a * b * c = a * (b * c) := by
+  -- Note: can be solved by `group`
   exact mul_assoc a b c
 
 example (a : G) : a * 1 = a := by
+  -- Note: can be solved by `group`
   exact LeftCancelMonoid.mul_one a
 
 example (a : G) : 1 * a = a := by
+  -- Note: can be solved by `group`
   exact LeftCancelMonoid.one_mul a
 
 example (a : G) : a * a⁻¹ = 1 := by
+  -- Note: can be solved by `group`
   exact mul_inv_self a
 
 variable (a b c : G)
 
 example : a⁻¹ * (a * b) = b := by
+  -- Note: can be solved by `exact inv_mul_cancel_left a b`
+  -- Note: can be solved by `group`
   rw [← mul_assoc]
   rw [inv_mul_self]
   exact LeftCancelMonoid.one_mul b
 
 example : a * (a⁻¹ * b) = b := by
+  -- Note: can be solved by `exact mul_inv_cancel_left a b`
+  -- Note: can be solved by `group`
   rw [← mul_assoc]
   rw [mul_inv_self]
   exact LeftCancelMonoid.one_mul b
 
 example {a b c : G} (h1 : b * a = 1) (h2 : a * c = 1) : b = c := by
-
   rw [← LeftCancelMonoid.mul_one (b * a)] at h1
   rw [← mul_inv_self c] at h1
   rw [mul_assoc] at h1
@@ -56,6 +63,7 @@ example {a b c : G} (h1 : b * a = 1) (h2 : a * c = 1) : b = c := by
 
 
 example : a * b = 1 ↔ a⁻¹ = b := by
+  -- Note: can be solved by `exact mul_eq_one_iff_inv_eq`
   constructor <;> intro h
 
   rw [← LeftCancelMonoid.mul_one (a⁻¹)]
@@ -69,36 +77,32 @@ example : a * b = 1 ↔ a⁻¹ = b := by
 
 
 example : (1 : G)⁻¹ = 1 := by
+  -- Note: can be solved by `exact inv_one`
+  -- Note: can be solved by `group`
   rw [← LeftCancelMonoid.mul_one (1⁻¹)]
   exact inv_mul_self 1
 
 example : a⁻¹⁻¹ = a := by
+  -- Note: can be solved by `exact DivisionMonoid.inv_inv a`
+  -- Note: can be solved by `group`
   rw [← LeftCancelMonoid.mul_one (a⁻¹⁻¹)]
   rw [← inv_mul_self a]
   rw [← mul_assoc]
   rw [inv_mul_self]
   exact LeftCancelMonoid.one_mul a
 
+
 example : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
-  sorry
+  -- Note: can be solved by `group`
+  exact DivisionMonoid.mul_inv_rev a b
 
 
-/-
-
-Remember the `ring` tactic which didn't look at hypotheses but which could
-prove hypothesis-free identities in commutative rings? There's also a `group`
-tactic which does the same thing for groups. This tactic would have solved
-many of the examples above.  NB the way it works is that it just uses
-Lean's simplifier but armed with all the examples above; a theorem of Knuth and Bendix
-says that these examples and the axioms of a group give a "confluent rewrite system"
-which can solve any identity in group theory. If you like you can
-try and prove the next example manually by rewriting with the lemmas above
-(if you know their names, which you can find out with `exact?` or by
-educated guessing).
-
--/
-example : (b⁻¹ * a⁻¹)⁻¹ * 1⁻¹⁻¹ * b⁻¹ * (a⁻¹ * a⁻¹⁻¹⁻¹) * a = 1 := by group
+example : (b⁻¹ * a⁻¹)⁻¹ * 1⁻¹⁻¹ * b⁻¹ * (a⁻¹ * a⁻¹⁻¹⁻¹) * a = 1 := by
+  group
 
 -- Try this trickier problem: if g^2=1 for all g in G, then G is abelian
 example (h : ∀ g : G, g * g = 1) : ∀ g h : G, g * h = h * g := by
-  sorry
+  intro e g
+
+  rw [← LeftCancelMonoid.mul_one (e * g)]
+  rw []
