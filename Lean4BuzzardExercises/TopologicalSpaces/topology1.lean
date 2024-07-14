2f36392ae64c3241ef9f0ec7cf5dc6f4b7b75e26
Pepa Montero Jimena <pepamonterojimena@gmail.com>
2024-07-14 17:05:16 +0200

    finished topology1

--- Lean4BuzzardExercises/TopologicalSpaces/topology1.lean
+++ Lean4BuzzardExercises/TopologicalSpaces/topology1.lean
@@ -76,35 +76,80 @@ example : TopologicalSpace X where
 
 
 
-
--- `isOpen_empty` is the theorem that in a topological space, the empty set is open.
--- Can you prove it yourself? Hint: arbitrary unions are open
-
 example (X : Type) [TopologicalSpace X] : IsOpen (∅ : Set X) := by
   -- `exact isOpen_empty`
+  have h1 : ∅ = ⋃₀ ∅
+  simp
+  rw [h1]
+  apply isOpen_sUnion
+  intro t ht
+  by_contra
+  exact ht
 
 
--- The reals are a topological space. Let's check Lean knows this already
+-- Topological space of ℝ
 #synth TopologicalSpace ℝ
 
--- Let's make it from first principles.
 
 def Real.IsOpen (s : Set ℝ) : Prop :=
-  -- every element of `s` has a neighbourhood (x - δ, x + δ) such that all y in this
-  -- neighbourhood are in `s`
   ∀ x ∈ s, ∃ δ > 0, ∀ y : ℝ, x - δ < y ∧ y < x + δ → y ∈ s
 
--- Now let's prove the axioms
+
 lemma Real.isOpen_univ : Real.IsOpen (Set.univ : Set ℝ) := by
-  sorry
+  intro x hx
+  use 1
+  constructor
+  norm_num
+  intro y hy
+  trivial
 
 lemma Real.isOpen_inter (s t : Set ℝ) (hs : IsOpen s) (ht : IsOpen t) : IsOpen (s ∩ t) := by
-  sorry
+  intro x hx
+  specialize hs x hx.left
+  specialize ht x hx.right
+  cases' hs with δ1 hδ1
+  cases' ht with δ2 hδ2
+  use min δ1 δ2
+  constructor
+  · exact lt_min hδ1.left hδ2.left
+  · intro y hy
+    cases' hy with hy1 hy2
+    constructor
+    · apply hδ1.right
+      have h1 : min δ1 δ2 ≤ δ1
+      exact min_le_left δ1 δ2
+      constructor
+      <;> linarith
+    · apply hδ2.right
+      have h2 : min δ1 δ2 ≤ δ2
+      exact min_le_right δ1 δ2
+      constructor
+      <;> linarith
+
 
 lemma Real.isOpen_sUnion (F : Set (Set ℝ)) (hF : ∀ s ∈ F, IsOpen s) : IsOpen (⋃₀ F) := by
-  sorry
+  intro x hx
+  cases' hx with s hs
 
--- now we put everything together using the notation for making a structure
+  have hs' : IsOpen s
+  apply hF
+  exact hs.left
+
+  specialize hs' x hs.right
+  cases' hs' with δ hδ
+  cases' hδ with h1 h2
+  use δ
+  constructor
+  · exact h1
+  · intro y hy
+    specialize h2 y hy
+    simp
+    use s
+    constructor
+    exact hs.left
+    exact h2
+
+
 example : TopologicalSpace ℝ where
   IsOpen := Real.IsOpen
   isOpen_univ := Real.isOpen_univ
